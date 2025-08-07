aeff2174a68883d47a0c115b3144a521f7a9697b
Dylan Coon <Dylan@Mac.lan>
2025-08-06 20:56:19 -0400

    video-process endpoint

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
+
+
+const app = express();
+app.use(express.json())
+
+
+app.post("/process-video", (req, res) => {
+	// Body contains the path of the input video file
+	const inputFilePath = req.body.inputFilePath;
+	const outputFilePath = req.body.outputFilePath;
+
+	if (!inputFilePath || !outputFilePath) {
+		// bad request
+		res.status(400).send("Bad request: Missing file path")
+	}
+
+	ffmpeg(inputFilePath)
+		.outputOptions("-vf", "scale=-1:360") // convert into 360p
+		// on the end event
+		.on("end", () => {
+			res.status(200).send("Video finished processing successfully.");
+		})
+		// on the error event
+		.on("error", (err) => {
+			console.log(`Error processing video: ${err.message}`);
+			res.status(500).send(`Error processing video: ${err.message}`);
+		})
+		.save(outputFilePath);
+
+	res.status(200).send("Video processing started");
+
+
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
+
